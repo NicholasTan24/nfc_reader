e52e4f58e0be40a94b753199904810310c198942
NicholasTan24 <nicholastan_2327240005@mhs.mdp.ac.id>
2026-03-02 15:41:48 +0700

    JSON project
    Membuat Project JSON

---
+++ karyawan/lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:karyawan/models/karyawan.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        foregroundColor: Colors.white,
+        title: const Text('Daftar Karyawan'),
+      ),
+      body: Center(child: Text('Data karyawan akan ditampilkan di sini')),
+    );
+  }
+}
