93bd26e87225377b2b0e2947166240f19d7a5399
tiaamelia35 <157791680+NicholasTan24@users.noreply.github.com>
2026-04-21 17:06:57 +0700

    perbaiki error
    Memperbaiki error flutter run

--- LatihanSebelumUTS-PAB2/mdp_gold/lib/firebase_options.dart
+++ LatihanSebelumUTS-PAB2/mdp_gold/lib/firebase_options.dart
@@ -23,15 +23,9 @@ class DefaultFirebaseOptions {
       case TargetPlatform.android:
         return android;
       case TargetPlatform.iOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for ios - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return ios;
       case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return macos;
       case TargetPlatform.windows:
         return windows;
       case TargetPlatform.linux:
@@ -74,4 +68,25 @@ class DefaultFirebaseOptions {
     databaseURL: 'https://gold-mdp-default-rtdb.firebaseio.com',
     storageBucket: 'gold-mdp.firebasestorage.app',
   );
+
+  static const FirebaseOptions macos = FirebaseOptions(
+    apiKey: 'AIzaSyD0rN3nn1qvGq2_lpjpWrat3uCHT8H6hrk',
+    appId: '1:880650508716:ios:4e576ab274523d8ab00209',
+    messagingSenderId: '880650508716',
+    projectId: 'gold-mdp',
+    databaseURL: 'https://gold-mdp-default-rtdb.firebaseio.com',
+    storageBucket: 'gold-mdp.firebasestorage.app',
+    iosBundleId: 'com.example.mdpGold',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyD0rN3nn1qvGq2_lpjpWrat3uCHT8H6hrk',
+    appId: '1:880650508716:ios:4e576ab274523d8ab00209',
+    messagingSenderId: '880650508716',
+    projectId: 'gold-mdp',
+    databaseURL: 'https://gold-mdp-default-rtdb.firebaseio.com',
+    storageBucket: 'gold-mdp.firebasestorage.app',
+    iosBundleId: 'com.example.mdpGold',
+  );
+
 }
